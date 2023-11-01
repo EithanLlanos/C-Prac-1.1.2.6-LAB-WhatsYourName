8f999328b11c9f856fd0e589ce9058d2d5bf9645
EithanLlanos <eithanllanosb@gmail.com>
2023-11-01 12:46:46 -0500

    Add project files.

---
+++ 1.1.2.6 LAB - What's your name.cpp
@@ -0,0 +1,13 @@
+// Write your first program in the "C" language.Just print your name 3 times.
+// Remember to include a return statement and make proper use of the main function.
+
+#include <stdio.h>
+
+
+
+int main(void){
+	puts("My name is Eithan");
+	puts("My name is Eithan");
+	puts("My name is Eithan");
+	return 0;
+	}
